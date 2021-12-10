a1c408c9dc0c6e5487b976040e50e51687e5832e
Calvin <calvindo95@gmail.com>
2021-12-10 14:05:02 -0600

    fixed zsh colors

--- Week1/Day1/spinup.sh
+++ Week1/Day1/spinup.sh
@@ -3,6 +3,5 @@
 # sets image name
 export docker_img="dockerdev"
 
-# mounts current working directory to /source_directory
-docker run -dit -v "$(pwd)":/home/calvin/source_directory:z -u $(whoami) -e "TERM=xterm-256color" $docker_img bash
-#docker cp $HOME/.zshrc $docker_img:$HOME
+# mounts local current working directory to /home/calvin/source_directory in the container
+docker run -dit -e COLORTERM=truecolor -v "$(pwd)":/home/calvin/source_directory:z -u $(whoami) $docker_img bash
