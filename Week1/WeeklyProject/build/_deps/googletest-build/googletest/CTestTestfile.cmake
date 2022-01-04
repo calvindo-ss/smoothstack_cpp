07fb5c0d9a61f893ba36624ef1fc44e2e2a5164f
Calvin <calvindo95@gmail.com>
2022-01-04 15:32:37 -0600

    idk

--- Week1/WeeklyProject/build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/calvin/MyProjects/smoothstack/Week1/WeeklyProject/build/_deps/googletest-src/googletest
-# Build directory: /home/calvin/MyProjects/smoothstack/Week1/WeeklyProject/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
