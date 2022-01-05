669e384e0af7428f65b12ab06a54a365435c6f78
Calvin <calvindo95@gmail.com>
2022-01-05 13:13:52 -0600

    refactored by changing singleton(ish) structure

--- Day10/assignment1/main.cpp
+++ Day10/assignment1/main.cpp
@@ -2,64 +2,79 @@
 #include <cassert>
 #include <string>
 
-// I cannot figure out how to delete the old Person pointer and return a new Person pointer
-// I am trying to overwrite the old pointer with a new pointer, but it does not work for me
-// My current workaround is to set the repeated object's name to "" and filter those out
-
 class Person { 
     private: 
-        static int _index;
-        
-        Person(std::string lname): name(lname){
-            for(int i = 0; i < _index; i++){
-                if(_persons[i] != nullptr){
-                    if(lname == _persons[i]->name){
-                    count += _persons[i]->count;
-                    _persons[i]->name = "";
-                    }
-                }
-                
-            }   
-        }
-
-        Person(){   // set object to nullptr if no name is provided
-            _persons[_index] = nullptr;
-        }
+        int index;
 
     public:
+        Person(std::string lname){
+            this->name = lname;
+
+            Person* p = getPerson(name);
+
+            // if object has nullptr, set object as this
+            if(p == nullptr) {
+                for(int i = 0; i < 10; i++) {
+                    if(_persons[i] == nullptr) {
+                        _persons[i] = this;
+                        index = i;
+                        break;
+                    }
+                }
+            } 
+            
+            // if matching object is found, increment count, set index to last matching index, overwrite old pointer with new one
+            else {
+                count += p->count;
+                index = p->index;
+                _persons[index] = this;
+                delete p;
+            }
+        };
+
         std::string name;
         int count = 1;
-        static Person* _persons[10];
+        static inline Person* _persons[10] {nullptr};
 
         static Person* getPerson(std::string lname){
-            if(lname != ""){
-                _persons[_index] = (Person*) new Person(lname);
-                _index++;
+            for(int j = 0; j < 10; j++) {
+                if(_persons[j] == nullptr)
+                    break;
+                    
+                if(_persons[j]->name == lname) // returns first object if matching name found
+                    return _persons[j];
             }
-            else{                
-            _persons[_index] = (Person*) new Person();
-            }
-            
-            return (Person*)(_persons[_index]);
+
+            return nullptr;
         }
 };
 
-int Person::_index = 0;
-Person *Person::_persons[] = {nullptr};
-
 int main(void) {
+    std::string names[10] = {"bob", "james", "janet", "bob", "stella", "bob", "bob", "james"};
 
-    std::string names[10] = {"bob", "james", "janet", "bob", "stella", "bob", "bob"};
     Person *person;
 
+    // create objects
     for(int i = 0; i < 10 ; i++){
-        person = Person::getPerson(names[i]);
+        person = new Person(names[i]);
     }
 
+    // displays data
     for (int ii = 0; ii < 10; ii++) {
-        if(Person::_persons[ii] != nullptr && Person::_persons[ii]->name != ""){
-            std::cout <<Person:: _persons[ii]->name << ": " << Person::_persons[ii]->count << ", ";
+        if(Person::_persons[ii] == nullptr || Person::_persons[ii]->name == ""){
+            break;
         }
-    }   
+
+        std::cout <<Person:: _persons[ii]->name << ": " << Person::_persons[ii]->count << ", ";
+    }
+
     std::cout << std::endl;
+
+
+    assert(Person:: _persons[0]->name == "bob");
+    assert(Person:: _persons[0]->count == 4);
+    assert(Person:: _persons[1]->name == "james");
+    assert(Person:: _persons[1]->count == 2);
+    assert(Person:: _persons[2]->name == "janet");
+    assert(Person:: _persons[2]->count == 1);
 }
