48409add90f5aa59802fb204dd265ebc8cdd8fda
Calvin <calvindo95@gmail.com>
2022-01-27 16:26:13 -0600

    removed redundant function

--- Day23/Assignment2/no_deadlock/main.cpp
+++ Day23/Assignment2/no_deadlock/main.cpp
@@ -135,7 +135,7 @@ void tests(){
     assert(round(c2.get_pounds()) == 1000000.000000);
 }
 
-void c1trades(std::chrono::steady_clock::time_point begin, Country& c1, Country& c2, Request r1, Request r2, Request r3, Request r4){
+void c_trades(std::chrono::steady_clock::time_point begin, Country& c1, Country& c2, Request r1, Request r2, Request r3, Request r4){
     double time;
     while(time < 10){
         int c = (std::rand() % 4);
@@ -159,30 +159,6 @@ void c1trades(std::chrono::steady_clock::time_point begin, Country& c1, Country&
     }
 }
 
-void c2trades(std::chrono::steady_clock::time_point begin, Country& c1, Country& c2, Request r1, Request r2, Request r3, Request r4){
-    double time;
-    while(time < 10){
-        int c = (std::rand() % 4);
-        switch(c){
-            case 0:
-                c2.trade(c1, r1);
-                break;
-            case 1:
-                c2.trade(c1, r2);
-                break;
-            case 2:
-                c2.trade(c1, r3);
-                break;
-            case 3:
-                c2.trade(c1, r4);
-                break;
-        }
-
-        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-        time = std::chrono::duration<double> (end - begin).count();
-    }
-}
-
 int main(void){
     tests();
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
@@ -195,8 +171,8 @@ int main(void){
     Request r3{0.74, 100000, GBP, BUY};
     Request r4{0.74, 100000, GBP, SELL};    
 
-    std::thread t1(c1trades, begin,std::ref(c1), std::ref(c2), r1, r2, r3, r4);
-    std::thread t2(c2trades, begin, std::ref(c2), std::ref(c1), r1, r2, r3, r4);
+    std::thread t1(c_trades, begin,std::ref(c1), std::ref(c2), r1, r2, r3, r4);
+    std::thread t2(c_trades, begin, std::ref(c2), std::ref(c1), r1, r2, r3, r4);
 
     t1.join();
     t2.join();
